73da178bd43b144643d6579f1bc5f511b5ebe638
Diana Gren <dianagren@gmail.com>
2015-03-27 17:21:27 -0700

    Setting up httpkit, making first requests.

--- src/labyrinth_app/core.clj
+++ src/labyrinth_app/core.clj
@@ -1,6 +1,16 @@
-(ns labyrinth-app.core)
+(ns labyrinth-app.core
+    (:require [org.httpkit.client :as http]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def options {:timeout 200
+              :headers {"X-Labyrinth-Email" "dianagren@gmail.com"}})
+(defn -main
+      [& args]
+      (let [{:keys [status header body error] :as resp} @(http/get "http://challenge2.airtime.com:7182/start" options)]
+           (if error
+             (println "Error " error)
+             (println "Success " status " results: " body))))
