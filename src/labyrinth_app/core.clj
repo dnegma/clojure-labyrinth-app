e0edcb93cba8217442c9a02ae8bb5b187b2dc8d7
Diana Gren <dianagren@gmail.com>
2015-03-27 17:34:16 -0700

    Add some tests, refactor.

--- src/labyrinth_app/core.clj
+++ src/labyrinth_app/core.clj
@@ -1,16 +1,19 @@
 (ns labyrinth-app.core
     (:require [org.httpkit.client :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (def options {:timeout 200
               :headers {"X-Labyrinth-Email" "dianagren@gmail.com"}})
-(defn -main
-      [& args]
-      (let [{:keys [status header body error] :as resp} @(http/get "http://challenge2.airtime.com:7182/start" options)]
+(def airtime-host
+  "http://challenge2.airtime.com:7182")
+
+(defn url [host path]
+      (clojure.string/join [host path]))
+
+(defn request [path]
+      (let [{:keys [status header body error] :as resp} @(http/get (url airtime-host path) options)]
            (if error
              (println "Error " error)
              (println "Success " status " results: " body))))
+(defn -main
+      [& args]
+      (request "/start"))
--- test/labyrinth_app/core_test.clj
+++ test/labyrinth_app/core_test.clj
@@ -2,6 +2,5 @@
   (:require [clojure.test :refer :all]
             [labyrinth-app.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest url-test
+         (is (= "www.google.com/test" (url "www.google.com" "/test"))))
