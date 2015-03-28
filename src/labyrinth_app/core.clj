2c36a9bb13cf9cdd42c78278e5b2fa0daedf5e65
Diana Gren <dianagren@gmail.com>
2015-03-28 10:06:50 -0700

    Create report with room ids and challenge code.

--- src/labyrinth_app/core.clj
+++ src/labyrinth_app/core.clj
@@ -1,40 +1,49 @@
 (ns labyrinth-app.core
-    (:require [org.httpkit.client :as http])
-    (:require [clojure.data.json :as json]))
+  (:require [org.httpkit.client :as http])
+  (:require [clojure.data.json :as json]))
 
 (defn options [params]
-           {:timeout 200
-            :query-params params
-            :headers {"X-Labyrinth-Email" "dianagren@gmail.com"}})
+  {:timeout 200
+   :query-params params
+   :headers {"X-Labyrinth-Email" "dianagren@gmail.com"}})
 
 (defn url [path]
-      (clojure.string/join ["http://challenge2.airtime.com:7182" path]))
+  (clojure.string/join ["http://challenge2.airtime.com:7182" path]))
 
 (defn request [path params]
-      (let [{:keys [status header body error] :as resp} @(http/get (url path) (options params))]
-           ;(println "path: " path " status: " status "params" params)
-           (json/read-str body :key-fn keyword)))
+  (let [{:keys [body]} @(http/get (url path) (options params))]
+    (json/read-str body :key-fn keyword)))
 
 (defn exits [room-id]
-      (:exits (request "/exits" {"roomId" room-id})))
+  (:exits (request "/exits" {"roomId" room-id})))
 
 (defn wall [room-id]
-      (request "/wall" {"roomId" room-id}))
+  (request "/wall" {"roomId" room-id}))
 
 (defn move [room-id exit]
-      (:roomId (request "/move" {"roomId" room-id "exit" exit})))
+  (:roomId (request "/move" {"roomId" room-id "exit" exit})))
 
 (def start
-      (request "/start" {}))
+  (request "/start" {}))
 
 (defn walk-all-rooms [room-id]
   (loop [exits (exits room-id)
-        walls []]
-       (if (empty? exits)
-         (conj walls (assoc (wall room-id) :roomId room-id))
-         (recur (rest exits)
-                (concat walls (walk-all-rooms (move room-id (first exits))))))))
+         walls []]
+    (if (empty? exits)
+      (conj walls (assoc (wall room-id) :roomId room-id))
+      (recur (rest exits)
+             (concat walls (walk-all-rooms (move room-id (first exits))))))))
+
+(defn is-dark? [room]
+  (and (= (:writing room) "xx") (= (:order room) -1)))
+
+(defn create-report [rooms]
+  (let [dark-rooms (filter is-dark? rooms)
+        light-rooms (remove is-dark? rooms)]
+    {:roomIds (map :roomId dark-rooms)
+     :challenge (map :writing light-rooms)}))
 
 (defn -main
-      [& args]
-      (println (walk-all-rooms (:roomId start))))
+  [& args]
+  (let [all-rooms (walk-all-rooms (:roomId start))]
+    (println (create-report all-rooms))))
