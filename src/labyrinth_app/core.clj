a6c0b08437bda283f68cc45c5cad3ffba3a72f8d
Diana Gren <dianagren@gmail.com>
2015-03-27 19:58:40 -0700

    Traversing all rooms, printing all wall writings.

--- src/labyrinth_app/core.clj
+++ src/labyrinth_app/core.clj
@@ -2,23 +2,17 @@
     (:require [org.httpkit.client :as http])
     (:require [clojure.data.json :as json]))
 
-(def host
-  "http://challenge2.airtime.com:7182")
-
 (defn options [params]
            {:timeout 200
             :query-params params
             :headers {"X-Labyrinth-Email" "dianagren@gmail.com"}})
 
-(def default-options
-  (options ""))
-
-(defn url [host path]
-      (clojure.string/join [host path]))
+(defn url [path]
+      (clojure.string/join ["http://challenge2.airtime.com:7182" path]))
 
 (defn request [path params]
-      (let [{:keys [status header body error] :as resp} @(http/get (url host path) (options params))]
-           (println "path: " path " status: " status)
+      (let [{:keys [status header body error] :as resp} @(http/get (url path) (options params))]
+           ;(println "path: " path " status: " status "params" params)
            (json/read-str body :key-fn keyword)))
 
 (defn exits [room-id]
@@ -28,7 +22,7 @@
       (request "/wall" {"roomId" room-id}))
 
 (defn move [room-id exit]
-      (request "/move" {"roomId" room-id "exit" exit}))
+      (:roomId (request "/move" {"roomId" room-id "exit" exit})))
 
 (def start
       (request "/start" {}))
@@ -36,15 +30,15 @@
 (defn find-black-rooms [room-id]
   (loop [new-exits (exits room-id)
         walls []]
-       (println "Exits; " new-exits)
+       ;(println "Exits; " new-exits "room-id" room-id)
        (if (empty? new-exits)
-         walls
+         (conj walls (wall room-id))
          (recur (rest new-exits)
                 (conj walls (find-black-rooms (move room-id (first new-exits)))))))
   )
 
 (defn -main
       [& args]
-      (println "Results " (:roomId start))
-      (println "Move " (move (:roomId start) "south"))
-      (find-black-rooms (:rommId start)))
+      ;(println "Results " (:roomId start))
+      ;(println "Move " (move (:roomId start) "south"))
+      (println (find-black-rooms (:roomId start))))
