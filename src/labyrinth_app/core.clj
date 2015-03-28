f48791f55632504690689f1fb4fee0bc9d5e4cab
Diana Gren <dianagren@gmail.com>
2015-03-28 11:22:04 -0700

    Working first version reporting results.

--- src/labyrinth_app/core.clj
+++ src/labyrinth_app/core.clj
@@ -2,23 +2,21 @@
   (:require [org.httpkit.client :as http])
   (:require [clojure.data.json :as json]))
 
-(defn options [query-params form-params]
+(def options
   {:timeout 200
-   :form-params form-params
-   :query-params query-params
    :headers {"X-Labyrinth-Email" "dianagren@gmail.com"}})
 
 (defn url [path]
   (clojure.string/join ["http://challenge2.airtime.com:7182" path]))
 
 (defn get-request [path params]
-  (let [{:keys [body]} @(http/get (url path) (options params {}))]
+  (let [{:keys [body]} @(http/get (url path) (assoc options :query-params params))]
     (json/read-str body :key-fn keyword)))
 
 (defn post-report [report]
   (println "Posting report " report)
-  (let [{:keys [status body]} @(http/post (url "/report") (options {} report))]
-    (println "Status " status "BODY " body)))
+  (let [post-options {:body (json/write-str report) :headers {"Content-Type" "application/json" "X-Labyrinth-Email" "dianagren@gmail.com"}} {:keys [body]} @(http/post (url "/report") post-options)]
+    body))
 
 (defn exits [room-id]
   (:exits (get-request "/exits" {"roomId" room-id})))
@@ -49,7 +47,7 @@
 (defn create-report [rooms]
   (let [dark-rooms (filter is-dark? rooms)
         light-rooms (remove is-dark? rooms)]
-    {:roomIds   (map :roomId dark-rooms)
+    {:roomIds (map :roomId dark-rooms)
      :challenge (clojure.string/join (map :writing (sort compare-rooms light-rooms)))}))
 
 (defn -main
